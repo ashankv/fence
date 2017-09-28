f977995b3d111fc7662eb4f4932f6c8a4dd754f3
Ashank Verma <ashank@wirelessprv-10-193-244-88.near.illinois.edu>
2017-09-27 19:24:07 -0500

    Set up colors, toolbar

---
+++ app/src/main/java/ex/org/fence/MainActivity.java
@@ -0,0 +1,20 @@
+package ex.org.fence;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolBar);
+        toolBar.setTitle("Fence");
+
+
+    }
+}
